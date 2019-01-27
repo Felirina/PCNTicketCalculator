1a95d1f3162ffa80debab9e0629d906602fc635f
Felirina <horvath.zsu52@gmail.com>
2019-01-27 16:22:23 +0000

    We finished the PCNData's constructor.

--- PCNTicketCalculator/PCNData.cs
+++ PCNTicketCalculator/PCNData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace PCNTicketCalculator
     {
         string licensePlate;
         DateTime entryDate;
-        DateTime exitDatep;
+        DateTime exitDate;
         string pcnReason;
         string pcnCandidateInfo;
         int calculatedSummary;
@@ -19,6 +20,18 @@ namespace PCNTicketCalculator
         // It's main task to create our class. It's name is same as the class name ( public class PCNData).
         public PCNData(string dataLine)
         {
+            //The split metod broken the string (string dataLine).
+            string[] tableBroken = dataLine.Split(new string[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
+
+            licensePlate = tableBroken[0];
+
+            // Sample: 23/01/2019 10:14
+            // CultureInfo.Invariant Culture = Do what I want (what I write :))
+            entryDate = DateTime.ParseExact(tableBroken[1], "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            exitDate = DateTime.ParseExact(tableBroken[2], "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            pcnReason = tableBroken[3];
+            pcnCandidateInfo = tableBroken[4];
 
         }
     }
