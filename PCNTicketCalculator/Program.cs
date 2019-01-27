cf51947d15447014f1934840d06401c1b2ef290f
Felirina <horvath.zsu52@gmail.com>
2019-01-27 17:04:19 +0000

    We created the file processing.

--- PCNTicketCalculator/Program.cs
+++ PCNTicketCalculator/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace PCNTicketCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<PCNData> pcnDataList = new List<PCNData>();
+
+            using (StreamReader sr = new StreamReader("RWY_Raw.txt"))
+            {
+                while (sr.EndOfStream == false)
+                {
+                    // We can read the file rows the data.
+                    string readText = sr.ReadLine();
+
+                    PCNData pcnData = new PCNData(readText);
+                    pcnDataList.Add(pcnData);
+                }
+
+            }
+
 
         }
     }
